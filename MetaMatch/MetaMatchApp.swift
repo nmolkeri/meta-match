66c6cf3c567c983a78bc82c082295698aeb17ef5
nmolkeri <nmolkeri@gmail.com>
2023-06-13 12:44:16 -0400

    Initial Commit

---
+++ MetaMatch/MetaMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaMatchApp.swift
+//  MetaMatch
+//
+//  Created by Niranjan on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
